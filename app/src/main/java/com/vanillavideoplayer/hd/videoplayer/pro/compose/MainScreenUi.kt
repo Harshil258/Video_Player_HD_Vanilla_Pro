1d273a921a0458a383660db8b1b539e69a67a792
Harshil vekariya <50706152+Harshil258@users.noreply.github.com>
2024-08-10 22:24:10 +0530

    backchange

--- app/src/main/java/com/vanillavideoplayer/hd/videoplayer/pro/compose/MainScreenUi.kt
+++ app/src/main/java/com/vanillavideoplayer/hd/videoplayer/pro/compose/MainScreenUi.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -32,14 +34,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RadialGradientShader
 import androidx.compose.ui.graphics.Shader
 import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -143,26 +152,92 @@ fun MainScreenUi(
     }
 
 
-
 }
 
+
 @Composable
-private fun HomeScreenBackground(
-    modifier: Modifier = Modifier,
-    content: @Composable BoxScope.() -> Unit
-) {
-    Box(
+private fun HomeScreenBackground(modifier: Modifier = Modifier, content: @Composable BoxScope.() -> Unit) {
+    BoxWithConstraints(
         modifier = modifier
-            .background(MaterialTheme.colorScheme.background)
+            .background(Color(0xFFD0E4FF))
+            .fillMaxSize()
     ) {
+        var width = maxWidth
+        var height = maxHeight
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .radialGradientScrim(MaterialTheme.colorScheme.primary.copy(alpha = 0.10f))
+                .radialGradientScrim(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    center = calculateRadialGradientCenter(with(LocalDensity.current) { width.toPx() },
+                        with(LocalDensity.current) { -(height / (0.40.dp)) }), radius = width - 50.dp
+                )
         )
         content()
     }
 }
+
+
+@PreviewLightDark
+@Composable
+fun RadialGradientPreview() {
+
+    BoxWithConstraints(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) {
+        var width = maxWidth
+        var height = maxHeight
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .radialGradientScrim(
+                    color = Color(0xFF0083E9),
+                    center = calculateRadialGradientCenter(with(LocalDensity.current) { width.toPx() },
+                        with(LocalDensity.current) { -(height / (0.40.dp)) }), radius = width - 50.dp
+                )
+        )
+    }
+}
+
+@Composable
+fun Modifier.radialGradientScrim(
+    color: Color, center: Offset = Offset(1f, 0f), // Top center (center horizontally, top vertically)
+    radius: Dp = 150.dp
+):
+        Modifier = this.then(Modifier.background(
+    brush = Brush.radialGradient(
+        colors = listOf(color, Color.Transparent), tileMode = TileMode.Decal,
+        center = center, radius = with(LocalDensity.current) { radius.toPx() } // Convert Dp to pixels,
+    )))
+
+@Composable
+fun calculateRadialGradientCenter(boxWidthPx: Float, boxHeightPx: Float): Offset {
+    val centerX = boxWidthPx / 2f // Center horizontally
+    val centerY = 0.23f * boxHeightPx // Top vertically (adjust percentage as needed)
+    return Offset(centerX, centerY)
+}
+
+
+//@Composable
+//private fun HomeScreenBackground(
+//    modifier: Modifier = Modifier,
+//    content: @Composable BoxScope.() -> Unit
+//) {
+//    Box(
+//        modifier = modifier
+//            .background(MaterialTheme.colorScheme.background)
+//    ) {
+//        Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .radialGradientScrim(MaterialTheme.colorScheme.primary.copy(alpha = 0.10f))
+//        )
+//        content()
+//    }
+//}
 
 
 fun Modifier.radialGradientScrim(color: Color): Modifier {
